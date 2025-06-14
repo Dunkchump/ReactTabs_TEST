a706f050b2139665c6f2cb597d228e97f235595d
Dunkchump <158578469+Dunkchump@users.noreply.github.com>
2025-06-14 16:04:47 +0300

    Horizontal scroll

--- src/components/Tabs/TabsContainer.jsx
+++ src/components/Tabs/TabsContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import TabItem from "./TabItem";
 import TabsDropdown from "./TabsDropdown";
 import { useTabsManager } from "../../hooks/useTabsManager";
@@ -24,17 +24,17 @@ const TabsContainer = () => {
   console.log("Tabs in TabsContainer:", tabs);
 
   const tabsListRef = useRef(null);
-  const tabRefs = useRef({});
+  const [tabRefs, setTabRefs] = useState({});
 
   useEffect(() => {
+    const refs = {};
     tabs.forEach((tab) => {
-      if (!tabRefs.current[tab.id]) {
-        tabRefs.current[tab.id] = React.createRef();
-      }
+      refs[tab.id] = refs[tab.id] || React.createRef();
     });
+    setTabRefs(refs);
   }, [tabs]);
 
-  const { visibleTabs, hiddenTabs } = useTabsResize(tabs, 200, 40);
+  const { hiddenTabs } = useTabsResize(tabs, tabRefs, tabsListRef);
 
   const {
     draggedItem,
@@ -47,14 +47,24 @@ const TabsContainer = () => {
     handleDrop,
   } = useDragAndDrop(tabs, reorderTabs);
 
+  const pinnedTabs = tabs.filter((tab) => tab.isPinned);
+  const unpinnedTabs = tabs.filter((tab) => !tab.isPinned);
+
   if (!tabs.length) {
     return null;
   }
 
   return (
-    <div className={`${styles.tabsContainer} tabs-container`}>
-      <div className={styles.tabsList} ref={tabsListRef}>
-        {visibleTabs.map((tab) => (
+    <div
+      className={`${styles.tabsContainer} tabs-container`}
+      style={{ display: "flex", alignItems: "center" }}
+    >
+      <div
+        className={styles.tabsList}
+        ref={tabsListRef}
+        style={{ flex: 1, minWidth: 0 }}
+      >
+        {pinnedTabs.map((tab) => (
           <TabItem
             key={tab.id}
             tab={tab}
@@ -70,15 +80,18 @@ const TabsContainer = () => {
             onDragEnter={handleDragEnter}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
-            ref={tabRefs.current[tab.id]}
+            ref={tabRefs[tab.id]}
           />
         ))}
-        {hiddenTabs.length > 0 && (
-          <TabsDropdown
-            tabs={hiddenTabs}
-            activeTabId={activeTabId}
-            onActivateTab={activateTab}
-            onCloseTab={closeTab}
+        {unpinnedTabs.map((tab) => (
+          <TabItem
+            key={tab.id}
+            tab={tab}
+            isActive={tab.id === activeTabId}
+            isDragged={draggedItem?.id === tab.id}
+            isDraggedOver={draggedOverItem?.id === tab.id}
+            onActivate={activateTab}
+            onClose={closeTab}
             onTogglePin={togglePinTab}
             onDragStart={handleDragStart}
             onDragEnd={handleDragEnd}
@@ -86,9 +99,25 @@ const TabsContainer = () => {
             onDragEnter={handleDragEnter}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
+            ref={tabRefs[tab.id]}
           />
-        )}
+        ))}
       </div>
+      {hiddenTabs.length > 0 && (
+        <TabsDropdown
+          tabs={hiddenTabs}
+          activeTabId={activeTabId}
+          onActivateTab={activateTab}
+          onCloseTab={closeTab}
+          onTogglePin={togglePinTab}
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        />
+      )}
     </div>
   );
 };
